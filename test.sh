76d51698a3ebab9b31fec2236b09d4a832df541c
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2018-09-22 18:20:31 +0100

    Add script to facilitate testing locally

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 echo "Building..."
 bundle exec jekyll build -s ./ -d ./_site --future --drafts
 echo "Starting to serve..."
-bundle exec jekyll serve -s ./ -d ./_site --future --drafts --unpublished
+bundle exec jekyll serve -s ./ -d ./_site --future --drafts --unpublished --host=0.0.0.0
 echo "Ready!"
 echo "Openning the page."
