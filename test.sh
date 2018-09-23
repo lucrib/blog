3e2de4309d77f3c83ea8f3caa3e7e0ffe9e6943b
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2018-09-23 12:44:26 +0100

    Fix profile images

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Building..."
-# bundle exec jekyll build -s ./ -d ./_site --future --drafts
+bundle exec jekyll build -s ./ -d ./_site --future --drafts
 echo "Starting to serve..."
 bundle exec jekyll serve -s ./ -d ./_site --future --drafts --unpublished --host=0.0.0.0
 echo "Ready!"
