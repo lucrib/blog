46a0db4bfe97215c12bf141db9c0b28b06f808d6
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2018-09-23 18:48:56 +0100

    Delete markdown-writer

--- docs/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-echo "Building..."
-bundle exec jekyll build -s ./ -d ./docs
-echo "Starting to serve..."
-bundle exec jekyll serve -s ./ -d ./docs
-echo "Ready!"
-echo "Openning the page."
